4f3d26512e0a2f6a72caebced33f76a678a487b7
TomekSzaty <75204970+TomekSzaty@users.noreply.github.com>
2022-11-23 18:13:44 +0100

    Added link navigation and 404 component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Customers from './pages/Customers';
 import Dictionary from './pages/Dictionary';
 import Definition from './pages/Definition';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -18,10 +19,12 @@ function App() {
       <Routes>
         <Route path='/employees' element={<Employees />} />
         <Route path='/dictionary' element={<Dictionary />} />
+        <Route path='/notfound' element={<NotFound />} />
+        <Route path='*' element={<NotFound />} />
         <Route 
               path='/definition/:search' 
               element={<Definition />} 
-        />
+              />
         <Route path='/customers' element={<Customers />} />
       </Routes>
     </Header>
--- src/pages/Dictionary.js
+++ src/pages/Dictionary.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function Dictionary() {
     const[word, setWord] = useState('');
@@ -13,12 +13,13 @@ export default function Dictionary() {
                     setWord(e.target.value);
                 }}
             />
-            <button 
+            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4'
                 onClick={()=>{
-                    navigate("/definition/" + word);
+                    navigate("/definition/" + word, {replace: true});
                 }}>
                 Search
             </button>
+             
            
         </>
     );
